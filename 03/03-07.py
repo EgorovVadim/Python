90352519929d885d17c9d49d47cd317a9ecf648d
QuadeLuna <6048060480dflbv>
2022-03-26 23:55:07 +0500

    First change

--- 03/03-07.py
+++ 03/03-07.py
@@ -1,24 +1,18 @@
-tries = 10
-cur_tries = tries
-max_number = 11
-answer = ""
-cur_number = max_number // 2
-preveous_number = max_number
-temp_preveous_number = 0
-while answer != "=" or cur_tries > tries:
-    print(cur_number)
-    answer = input()
-    tries -= 1
-    if answer == "<":
-        temp = abs(cur_number - preveous_number) // 2
-        preveous_number = cur_number
-        cur_number -= temp
-    if answer == ">":
-        temp = abs(cur_number - preveous_number) // 2
-        if temp_preveous_number > cur_number:
-            preveous_number = temp_preveous_number
-        else:
-            preveous_number = cur_number
-        cur_number += temp
-    temp_preveous_number = preveous_number
-    print("Осталось попыток: ", tries)
+lives = 10
+left_border = right_border = temp_border = 0
+a = ""
+cur_position = int(1000 / 2)
+while lives > 0:
+    print("Осталось попыток : ", lives)
+    print(cur_position)
+    a = input("Введите знак: ")
+    if a == "<":
+        right_border = cur_position
+        temp_border = cur_position
+    elif a == ">":
+        left_border = cur_position
+        right_border = temp_border
+    elif a == "=":
+        break
+    cur_position = (left_border + right_border) // 2
+    lives -= 1
